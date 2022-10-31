51cae835174baadfa1df9f1051ee187ea93ee864
remon2207 <reimond2207@gmail.com>
2022-10-31 12:53:50 +0900

    refactor: Remove commentout line

--- frontend/src/components/atoms/Headers/CreateBtn.tsx
+++ frontend/src/components/atoms/Headers/CreateBtn.tsx
@@ -7,9 +7,6 @@ type Props = {
 export const CreateBtn: React.FC<Props> = ({ className }) => {
   return (
     <>
-      {/* <button className={className} type="button"> */}
-      {/*   新規作成 */}
-      {/* </button> */}
       <Link className={className} href="/new">
         新規作成
       </Link>
