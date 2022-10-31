21db6400b90a81cfec6d60ff6a3c24b5764e8ac8
remon2207 <reimond2207@gmail.com>
2022-10-31 12:55:07 +0900

    style: Remove blank line

--- frontend/src/utils/apollo-client.ts
+++ frontend/src/utils/apollo-client.ts
@@ -3,7 +3,6 @@ import { ApolloClient, InMemoryCache } from '@apollo/client'
 const cache = new InMemoryCache()
 const uri = 'http://api:4000/graphql'
 
-
 export const client = new ApolloClient({
   cache,
   uri
