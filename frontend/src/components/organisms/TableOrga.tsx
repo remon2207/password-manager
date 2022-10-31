f10e6760bd53466943a2479338aa0b82995a7b77
remon2207 <reimond2207@gmail.com>
2022-10-31 19:24:54 +0900

    feat: Add style with Tailwind CSS

--- frontend/src/components/organisms/TableOrga.tsx
+++ frontend/src/components/organisms/TableOrga.tsx
@@ -1,14 +1,14 @@
-import { TableBodyMole } from "components/molecules/TableBodyMole"
-import { TableHeaderMole } from "components/molecules/TableHeaderMole"
+import { TableBodyMole } from 'components/molecules/TableBodyMole'
+import { TableHeaderMole } from 'components/molecules/TableHeaderMole'
 
 export const TableOrga: React.FC = () => {
   return (
     <>
-      <table>
+      <table className="mx-auto w-full table-auto lg:w-full xl:w-1/2">
         <thead>
           <TableHeaderMole />
         </thead>
-          <tbody>
+        <tbody>
           <TableBodyMole />
         </tbody>
       </table>
