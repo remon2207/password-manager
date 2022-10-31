79af7ce8fae8d53d16cb2af829583fdd435ec274
remon2207 <reimond2207@gmail.com>
2022-10-31 14:27:42 +0900

    feat: Add getServerSideProps function

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,13 +1,63 @@
+import { gql } from '@apollo/client'
+
 import { HomeTemp } from 'components/templates/HomeTemp'
+import { client } from 'utils/apollo-client'
 
-import type { NextPage } from 'next'
+import type { GetServerSideProps, NextPage } from 'next'
 
-const Home: NextPage = () => {
+const getPws = gql`
+  query Query($userId: Int!) {
+    getPws(userId: $userId) {
+      id
+      userId
+      service
+      email
+      name
+      password
+      twoFactor
+      secret
+    }
+  }
+`
+
+type Props = GetPws
+
+const Home: NextPage<Props> = ({ getPws }) => {
+  const service = getPws.map((pw) => pw.service)
   return (
     <>
       <HomeTemp />
+      {service}
     </>
   )
 }
 
 export default Home
+
+type GetPws = {
+  getPws: [
+    {
+      id: number
+      userId: number
+      service: string
+      email: string
+      name: string
+      password: string
+      twoFactor: boolean
+      secret: string
+    }
+  ]
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const { data } = await client.query<GetPws>({
+    query: getPws,
+    variables: { userId: 1 }
+  })
+
+  return {
+    props: {
+      getPws: data.getPws
+    }
+  }
+}
