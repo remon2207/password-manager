5d3489ef8108d56170f2e5b657fb3af6651916c3
remon2207 <reimond2207@gmail.com>
2022-10-31 12:46:25 +0900

    feat: Add wrap the ApolloProvider

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,11 +1,26 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import '../styles/globals.css'
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+} from '@apollo/client'
+
 import type { AppProps } from 'next/app'
+
+const cache = new InMemoryCache()
+const uri = 'http://api:4000/graphql'
+export const client = new ApolloClient({
+  uri,
+  cache
+})
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <>
-      <Component {...pageProps} />
+      <ApolloProvider client={client}>
+        <Component {...pageProps} />
+      </ApolloProvider>
     </>
   )
 }
