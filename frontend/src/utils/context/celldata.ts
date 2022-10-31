e84177f1208c5e58199caba4cbe54a08b1ea81b5
remon2207 <reimond2207@gmail.com>
2022-10-31 19:25:27 +0900

    feat: Add useContext for cell

---
+++ frontend/src/utils/context/celldata.ts
@@ -0,0 +1,17 @@
+import { createContext } from "react";
+
+const defaultValue = [
+  {
+    id: 0,
+    userId: 0,
+    service: '',
+    email: '',
+    name: '',
+    password: '',
+    twoFactor: false,
+    secret: ''
+  }
+]
+
+
+export const CellDataContext = createContext(defaultValue)
