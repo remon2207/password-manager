2b590eb30b8dd84716b2c478e34320bef784f94a
remon2207 <reimond2207@gmail.com>
2022-10-31 12:54:17 +0900

    refactor: Split files

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,19 +1,11 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import '../styles/globals.css'
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-} from '@apollo/client'
+
+import { ApolloProvider } from '@apollo/client'
+
+import { client } from 'utils/apollo-client'
 
 import type { AppProps } from 'next/app'
-
-const cache = new InMemoryCache()
-const uri = 'http://api:4000/graphql'
-export const client = new ApolloClient({
-  uri,
-  cache
-})
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
---
+++ frontend/src/utils/apollo-client.ts
@@ -0,0 +1,10 @@
+import { ApolloClient, InMemoryCache } from '@apollo/client'
+
+const cache = new InMemoryCache()
+const uri = 'http://api:4000/graphql'
+
+
+export const client = new ApolloClient({
+  cache,
+  uri
+})
