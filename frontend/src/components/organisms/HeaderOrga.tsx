566f41437ee3a19179b99ce60cf8e9ca8d841b4f
remon2207 <reimond2207@gmail.com>
2022-11-01 10:33:07 +0900

    refactor: Add wrap with nav and header tag

--- frontend/src/components/organisms/HeaderOrga.tsx
+++ frontend/src/components/organisms/HeaderOrga.tsx
@@ -3,9 +3,11 @@ import { HeaderMole } from 'components/molecules/HeaderMole'
 export const HeaderOrga: React.FC = () => {
   return (
     <>
-      <div className="flex h-24 flex-row items-center justify-center bg-gray-200">
-        <HeaderMole />
-      </div>
+      <header>
+        <nav className="flex h-24 flex-row items-center justify-center bg-gray-200">
+          <HeaderMole />
+        </nav>
+      </header>
     </>
   )
 }
