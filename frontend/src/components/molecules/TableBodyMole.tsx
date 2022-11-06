0f82270b97cc1ffea11ec02a48fa6bc54fbfdda8
remon2207 <reimond2207@gmail.com>
2022-11-06 17:05:06 +0900

    feat: Split file

--- frontend/src/components/molecules/TableBodyMole.tsx
+++ frontend/src/components/molecules/TableBodyMole.tsx
@@ -1,34 +1,16 @@
-import { useContext } from 'react'
-
 import { BodyCell } from 'components/atoms/tables/BodyCell'
-import { InputCell } from 'components/atoms/tables/InputCell'
-import { CellDataContext } from 'utils/context/celldata'
 
-export const TableBodyMole: React.FC = () => {
-  const cells = useContext(CellDataContext)
+type Props = {
+  text?: string | boolean
+  children?: React.ReactNode
+}
 
-  const handleFocusAllSelect = (e: React.FocusEvent<HTMLInputElement>) =>
-    e.target.select()
-
+export const TableBodyMole: React.FC<Props> = ({ text, children }) => {
   return (
     <>
-      {cells.map((cell) => (
-        <tr key={cell.id}>
-          <BodyCell className="table-cell" text={cell.service} />
-          <BodyCell className="table-cell" text={cell.email} />
-          <BodyCell className="table-cell" text={cell.name} />
-          <BodyCell className="table-cell">
-            <InputCell
-              className="border-none outline-none"
-              onFocus={handleFocusAllSelect}
-              size={10}
-              value={cell.password}
-            />
-          </BodyCell>
-          <BodyCell className="table-cell" text={cell.twoFactor} />
-          <BodyCell className="table-cell" text={cell.secret} />
-        </tr>
-      ))}
+      <BodyCell className="table-cell" text={text}>
+        {children}
+      </BodyCell>
     </>
   )
 }
--- frontend/src/components/organisms/TableOrga.tsx
+++ frontend/src/components/organisms/TableOrga.tsx
@@ -1,7 +1,17 @@
+import { useContext } from 'react'
+
+import { InputCell } from 'components/atoms/tables/InputCell'
 import { TableBodyMole } from 'components/molecules/TableBodyMole'
 import { TableHeaderMole } from 'components/molecules/TableHeaderMole'
+import { CellDataContext } from 'utils/context/cellData'
 
 export const TableOrga: React.FC = () => {
+  const cells = useContext(CellDataContext)
+
+  const handleFocus = (e: React.FocusEvent<HTMLInputElement>) => {
+    e.target.select()
+  }
+
   return (
     <>
       <table className="mx-auto w-full table-auto lg:w-full xl:w-1/2">
@@ -9,7 +19,23 @@ export const TableOrga: React.FC = () => {
           <TableHeaderMole />
         </thead>
         <tbody>
-          <TableBodyMole />
+          {cells.map((cell) => (
+            <tr key={cell.id}>
+              <TableBodyMole text={cell.service} />
+              <TableBodyMole text={cell.email} />
+              <TableBodyMole text={cell.name} />
+              <TableBodyMole>
+                <InputCell
+                  className="focus:outline-none"
+                  onFocus={handleFocus}
+                  size={10}
+                  value={cell.password}
+                />
+              </TableBodyMole>
+              <TableBodyMole text={cell.twoFactor} />
+              <TableBodyMole text={cell.secret} />
+            </tr>
+          ))}
         </tbody>
       </table>
     </>
