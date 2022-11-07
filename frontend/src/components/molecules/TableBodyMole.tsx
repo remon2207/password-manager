5c0949956fdd847df3daeebdd63282ea7e5f1017
remon2207 <reimond2207@gmail.com>
2022-11-07 12:10:07 +0900

    refactor: Remove text props

--- frontend/src/components/molecules/TableBodyMole.tsx
+++ frontend/src/components/molecules/TableBodyMole.tsx
@@ -1,14 +1,13 @@
 import { BodyCell } from 'components/atoms/tables/BodyCell'
 
 type Props = {
-  text?: string | boolean
   children?: React.ReactNode
 }
 
-export const TableBodyMole: React.FC<Props> = ({ text, children }) => {
+export const TableBodyMole: React.FC<Props> = ({ children }) => {
   return (
     <>
-      <BodyCell className="table-cell" text={text}>
+      <BodyCell className="table-cell">
         {children}
       </BodyCell>
     </>
