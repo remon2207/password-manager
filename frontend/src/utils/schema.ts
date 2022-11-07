f5dda43d0ec181f7c9e84289a372d622f4182e21
remon2207 <reimond2207@gmail.com>
2022-11-07 16:26:13 +0900

    refactor: Add typeDefs

--- frontend/src/utils/schema.ts
+++ frontend/src/utils/schema.ts
@@ -1,3 +1,4 @@
+import { gql } from '@apollo/client'
 import * as yup from 'yup'
 
 export const formSchema = yup
@@ -10,3 +11,66 @@ export const formSchema = yup
     secret: yup.string()
   })
   .required()
+
+export const typeDefs = gql`
+  type Query {
+    getUsers: [User]!
+    getUser(id: Int!): User!
+    getPws(userId: Int!): [PwInfo]!
+    getPw(id: Int!): PwInfo!
+  }
+
+  type Mutation {
+    userRegister(user: NewUserInput!): Message!
+    userDeleter(id: Int!): Message!
+    pwRegister(pw: NewPwInfoInput!): Message!
+    pwUpdater(pw: UpdatePwInfoInput!): Message!
+    pwDeleter(id: Int!): Message!
+  }
+
+  type Message {
+    message: String!
+  }
+
+  type User {
+    id: Int!
+    name: String!
+    email: String!
+  }
+
+  type PwInfo {
+    id: Int!
+    userId: Int!
+    service: String!
+    email: String!
+    name: String!
+    password: String!
+    twoFactor: Boolean!
+    secret: String!
+  }
+
+  input NewUserInput {
+    name: String!
+    email: String!
+  }
+
+  input NewPwInfoInput {
+    userId: Int!
+    service: String!
+    email: String!
+    name: String!
+    password: String!
+    twoFactor: Boolean!
+    secret: String!
+  }
+
+  input UpdatePwInfoInput {
+    id: Int!
+    service: String!
+    email: String!
+    name: String!
+    password: String!
+    twoFactor: Boolean!
+    secret: String!
+  }
+`
