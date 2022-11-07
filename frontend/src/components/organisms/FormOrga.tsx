c444010f15b6ab524968ff37e14c5a4c6a6e8bc7
remon2207 <reimond2207@gmail.com>
2022-11-07 12:23:16 +0900

    refactor: To small components

--- frontend/src/components/organisms/FormOrga.tsx
+++ frontend/src/components/organisms/FormOrga.tsx
@@ -1,72 +1,37 @@
-/* eslint-disable react/jsx-props-no-spreading */
-import { yupResolver } from '@hookform/resolvers/yup'
-import { useForm } from 'react-hook-form'
+import { ComponentPropsWithoutRef } from 'react'
+import { UseFormRegisterReturn } from 'react-hook-form'
 
 import { InputForm } from 'components/molecules/InputForm'
-import { useSubmit } from 'hooks/useSubmit'
-import { FormInput } from 'types/form'
-import { formSchema } from 'utils/schema'
 
-export const FormOrga: React.FC = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors }
-  } = useForm<FormInput>({
-    resolver: yupResolver(formSchema)
-  })
+type Props = ComponentPropsWithoutRef<'input'> & {
+  htmlFor: string
+  labelName: string
+  register: UseFormRegisterReturn
+  error?: string
+}
 
-  const [onSubmit] = useSubmit()
-
+export const FormOrga: React.FC<Props> = ({
+  htmlFor,
+  register,
+  labelName,
+  type,
+  error,
+  defaultValue,
+  checked
+}) => {
   return (
     <>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="flex flex-col items-center justify-center">
-          <InputForm
-            error={errors.service?.message}
-            htmlFor="service"
-            labelName="Service*"
-            register={register('service')}
-            required
-            type="text"
-          />
-          <InputForm
-            error={errors.email?.message}
-            htmlFor="email"
-            labelName="Email"
-            register={register('email')}
-            type="email"
-          />
-          <InputForm
-            htmlFor="name"
-            labelName="Name"
-            register={register('name')}
-            type="text"
-          />
-          <InputForm
-            htmlFor="password"
-            labelName="Password"
-            register={register('password')}
-            type="text"
-          />
-          <InputForm
-            htmlFor="two-factor"
-            labelName="Two factor"
-            register={register('twoFactor')}
-            type="checkbox"
-          />
-          <InputForm
-            htmlFor="secret"
-            labelName="Secret"
-            register={register('secret')}
-            type="text"
-          />
-          <div className="mt-14 flex flex-row items-center justify-center gap-24">
-            <input className="page-transition-btn" type="submit" value="Back" />
-            <input className="page-transition-btn" type="submit" value="Send" />
-          </div>
-        </div>
-      </form>
+      <div className="flex flex-col items-center justify-center">
+        <InputForm
+          checked={checked}
+          defaultValue={defaultValue}
+          error={error}
+          htmlFor={htmlFor}
+          labelName={labelName}
+          register={register}
+          type={type}
+        />
+      </div>
     </>
   )
 }
