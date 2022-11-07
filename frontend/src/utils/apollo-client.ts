959d96ddc8a401be56ad12071316bd569f33251c
remon2207 <reimond2207@gmail.com>
2022-11-07 10:01:33 +0900

    fix: Change uri with CSR or SSR

--- frontend/src/utils/apollo-client.ts
+++ frontend/src/utils/apollo-client.ts
@@ -1,7 +1,11 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 const cache = new InMemoryCache()
-const uri = 'http://api:4000/graphql'
+
+const uri =
+  typeof window !== 'undefined'
+    ? 'http://localhost:4000/graphql'
+    : 'http://api:4000/graphql'
 
 export const client = new ApolloClient({
   cache,
