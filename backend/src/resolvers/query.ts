d7e0e9a39042508f527f88b5cca4087f1046eec7
remon2207 <reimond2207@gmail.com>
2022-11-13 18:03:00 +0900

    fix: arguments for getUserId

--- backend/src/resolvers/query.ts
+++ backend/src/resolvers/query.ts
@@ -6,7 +6,7 @@ import type { QueryResolvers } from 'generated/graphql'
 export const query: QueryResolvers = {
   getUsers: () => getUsers(),
   getUser: (_, { id }) => getUser(id),
-  getUserId: (_, { id }) => getUserId(id),
+  getUserId: (_, { name, email }) => getUserId(name, email),
   getPws: (_, { userId }) => getPws(userId),
   getPw: (_, { id }) => getPw(id)
 }
