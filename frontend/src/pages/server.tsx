44f7ac557ce3207aed4c03a3b09c20173158bc00
remon2207 <reimond2207@gmail.com>
2022-11-19 16:09:02 +0900

    fix: Update user id with /server

--- frontend/src/pages/server.tsx
+++ frontend/src/pages/server.tsx
@@ -1,13 +1,23 @@
+import { useContext, useEffect } from 'react'
+
 import { ServerTemp } from 'components/templates'
 import { GetServers } from 'types/server'
-import { ServerCellDataContext } from 'utils'
+import { ServerCellDataContext, SetUserIdContext } from 'utils'
 import { checkSession } from 'utils/checkSession'
 
 import type { GetServerSideProps, NextPage } from 'next'
 
-type Props = GetServers
+type Props = GetServers & {
+  userId: number
+}
 
-const Server: NextPage<Props> = ({ getServers }) => {
+const Server: NextPage<Props> = ({ getServers, userId }) => {
+  const setUserId = useContext(SetUserIdContext)
+
+  useEffect(() => {
+    setUserId(userId)
+  }, [userId, setUserId])
+
   return (
     <>
       <ServerCellDataContext.Provider value={getServers}>
