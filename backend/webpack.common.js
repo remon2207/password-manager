31385a5968ecc3c659b9f16d07e0b825ca05c8b0
remon2207 <reimond2207@gmail.com>
2022-11-28 11:40:50 +0900

    chore: Change filename

--- backend/webpack.common.js
+++ backend/webpack.common.js
@@ -2,10 +2,10 @@ const path = require('path')
 const nodeExternals = require('webpack-node-externals')
 
 const config = {
-  entry: path.resolve(__dirname, 'dist/index.js'),
+  entry: path.resolve(__dirname, 'src/index.ts'),
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'index.js'
   },
   externalsPresets: {
     node: true
